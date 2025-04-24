7486d0d4b9db34cee0e8d54e49dc8e54cd9ea947
4ouChan <kumho3983398@naver.com>
2025-04-24 15:33:53 +0900

    Feat: 메뉴를 출력하는 기능. 번호 입력 시 번호에 맞게 상품을 출력하는 기능. 특정 번호 입력 시 프로그램이 종료되는 기능.

--- src/kiosk/lv1/Main.java
+++ src/kiosk/lv1/Main.java
@@ -1,8 +1,80 @@
 package kiosk.lv1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
 
+    String burgerName;
+    String price;
+    String burgerAccount;
 
+    public String shackBurger() {
+        burgerName = "1. shackBurger  ";
+        price = " | W 6.9 | ";
+        burgerAccount = "토마토, 양상추, 쉑소스가 토핑된 치즈버거";
+        return burgerName + price + burgerAccount;
+    }
+
+    public String smokeShack() {
+        burgerName = "2. SmokeShack   ";
+        price = " | W 8.9 | ";
+        burgerAccount = "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거";
+        return burgerName + price + burgerAccount;
+    }
+
+    public String cheeseburger() {
+        burgerName = "3. Cheeseburger ";
+        price = " | W 6.9 | ";
+        burgerAccount = "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거";
+        return burgerName + price + burgerAccount;
+    }
+
+    public String hamburger() {
+        burgerName = "4. Hamburger    ";
+        price = " | W 5.4 | ";
+        burgerAccount = "비프패티를 기반으로 야채가 들어간 기본버거";
+        return burgerName + price + burgerAccount;
+    }
+
+    public static void main(String[] args) {
+
+        Main burger = new Main();
+        Scanner scanner = new Scanner(System.in);
+        char userText = '1';
+        String printBuger;
+
+        while (userText != '0') {
+            System.out.println("\n[ SHAKESHACK MENU ]");
+
+            printBuger = burger.shackBurger();
+            System.out.println(printBuger);
+            printBuger = burger.smokeShack();
+            System.out.println(printBuger);
+            printBuger = burger.cheeseburger();
+            System.out.println(printBuger);
+            printBuger = burger.hamburger();
+            System.out.println(printBuger);
+            System.out.println("0. 종료       | 종료\n");
+
+            System.out.print("원하시는 번호를 입력해주십시오. (처음 입력한 숫자만 인식합니다.): ");
+            userText = scanner.next().charAt(0);
+
+            switch (userText) {
+                case '1' -> System.out.println("\n선택한 제품: " + burger.shackBurger());
+                case '2' -> System.out.println("\n선택한 제품: " + burger.smokeShack());
+                case '3' -> System.out.println("\n선택한 제품: " + burger.cheeseburger());
+                case '4' -> System.out.println("\n선택한 제품: " + burger.hamburger());
+                case '0' -> {
+                    System.out.println("\n프로그램을 종료합니다. ");
+                    continue;
+                }
+                default -> {
+                    System.out.print("잘못된 입력입니다.");
+                    continue;
+                }
+            }
+        }
+    }
 }
+
+
